04462dbca17b97200bd57495cb38c854cf84ca7b
Augusto Penzo Jara <115497193+AhGusto04@users.noreply.github.com>
2026-04-08 19:59:03 -0600

    Delete orbit_controller.gd

--- orbit_controller.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-@onready var player = $player
-@onready var charge_circle = $UI/CanvasLayer/CenterContainer/CrosshairRoot/ChargeCircle
-
-func _ready():
-	player.charge_ui = charge_circle
