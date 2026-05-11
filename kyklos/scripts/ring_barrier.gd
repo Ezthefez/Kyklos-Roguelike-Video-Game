6e0b981d143635b74a2a06328286e3fcdad835cf
Augusto Penzo Jara <augustoj@nmsu.edu>
2026-05-11 06:03:26 -0600

    Level select description, ring barrier, projectile tweaks, orbiting target spheres

---
+++ kyklos/scripts/ring_barrier.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+@export var spin_speed_x_deg: float = 16.0
+@export var spin_speed_y_deg: float = 22.0
+@export var spin_dir_x: float = -1.0
+@export var spin_dir_y: float = -1.0
+
+func _ready() -> void:
+	add_to_group("barrier")
+
+func _process(delta: float) -> void:
+	rotation.x += deg_to_rad(spin_speed_x_deg * spin_dir_x) * delta
+	rotation.y += deg_to_rad(spin_speed_y_deg * spin_dir_y) * delta
